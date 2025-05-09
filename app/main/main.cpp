d095c143c9615fdd6c9462564dccdc874a9e7823
Franciszek Janicki <franciszek.janicki1@gmail.com>
2025-05-09 12:46:36 +0200

    remove unnecessary stuff

--- app/main/main.cpp
+++ app/main/main.cpp
@@ -1,15 +1,10 @@
 #include "main.h"
-#include "usart.h"
 
 int main()
 {
     HAL_Init();
     SystemClock_Config();
 
-    MX_USART2_UART_Init();
-
-    while (true) {
+    while (1) {
     }
-
-    return 0;
 }
